ebc78f42417c7ece827e49609c26a48ed091617c
Bruno <bru_16@hotmail.com.ar>
2021-09-02 17:34:45 -0300

    create list from two ordered ones.

--- src/algoritmia/OrdenarListas.java
+++ src/algoritmia/OrdenarListas.java
@@ -9,10 +9,9 @@ public class OrdenarListas {
         ArrayList<Integer> B = new ArrayList();
 
         for (int i = 1; i <= 60; i++) {
-            A.add(i);
             B.add(i);
         }
-        for (int i = 61; i <= 100; i++) {
+        for (int i = 61; i <= 160; i++) {
             A.add(i);
         }
 
@@ -27,7 +26,9 @@ public class OrdenarListas {
         System.out.println("La lista A ORDENADA: " + A.toString());
         System.out.println("La lista B ORDENADA: " + B.toString());
 
-        ArrayList<Integer> C = new ArrayList<Integer>();
+        ArrayList<Integer> C = crearListaMezclada(A, B);
+
+        System.out.println("La lista C ORDENADA a partir de A y B: " + C.toString());
     }
 
     private static void ordenarListaBubbleSort(ArrayList<Integer> lista) {
@@ -61,4 +62,32 @@ public class OrdenarListas {
             lista.set(j + 1, key);
         }
     }
+
+    private static ArrayList<Integer> crearListaMezclada(ArrayList<Integer> lista1, ArrayList<Integer> lista2) {
+        ArrayList<Integer> listaNueva = new ArrayList<Integer>();
+        int i = 0, j = 0;
+        int listaUnoSize = lista1.size();
+        int listaDosSize = lista2.size();
+
+        // voy agregando correspondientemente hasta que una haya completado al menos una lista.
+        while (i < listaUnoSize && j < listaDosSize) {
+            if (lista1.get(i) < lista2.get(j)) {
+                listaNueva.add(lista1.get(i));
+                i++;
+            } else {
+                listaNueva.add(lista2.get(j));
+                j++;
+            }
+        }
+        while (i < listaUnoSize) {  // si quedan elementos en la primera lista, los agrego,
+            listaNueva.add(lista1.get(i));// ya que serian mayores a los ya agregados anteriormente
+            i++;
+        }
+        while (j < listaDosSize) {  // lo mismo aca
+            listaNueva.add(lista2.get(j));
+            j++;
+        }
+        return listaNueva;
+    }
+
 }
